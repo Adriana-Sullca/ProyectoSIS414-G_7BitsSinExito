2d4dd1338435dc04c7f2c7b26b8e36ba6979d369
Melani Hidalgo <marcahidalgo7@gmail.com>
2026-05-19 17:52:19 -0400

    Se agrega UnidadAdminEntity y actualización repository

---
+++ src/main/java/com/example/ProyectoSis414/entity/UnidadAdminEntity.java
@@ -0,0 +1,64 @@
+package com.example.ProyectoSis414.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "unidad_admin")
+public class UnidadAdminEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String entidad;
+    private String unidad;
+    private String descripcion;
+    private String ciudad;
+
+    public UnidadAdminEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEntidad() {
+        return entidad;
+    }
+
+    public void setEntidad(String entidad) {
+        this.entidad = entidad;
+    }
+
+    public String getUnidad() {
+        return unidad;
+    }
+
+    public void setUnidad(String unidad) {
+        this.unidad = unidad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+}
