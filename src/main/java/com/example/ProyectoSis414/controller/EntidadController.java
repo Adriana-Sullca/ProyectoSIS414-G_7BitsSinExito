0f846ac9f4154a8e27c4a6536221a28b7dc50729
Dafne Tapia <romidafne23@gmail.com>
2026-05-19 18:06:52 -0400

    feat: agregar EntidadEntity, EntidadRepository y configurar BD

--- src/main/java/com/example/ProyectoSis414/controller/EntidadController.java
+++ src/main/java/com/example/ProyectoSis414/controller/EntidadController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/entidades")
+@CrossOrigin(origins = "*")
 public class EntidadController {
 
     private final EntidadService entidadService;
